b12b97f54fe273525e9aae70b13c8824c779915f
LeandroAugusto16 <leandroaugustobap@gmail.com>
2024-08-23 20:51:08 -0300

    Criação de projeto

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+
+  
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
